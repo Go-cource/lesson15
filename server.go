b86a0ef6cf5061441d48243569d23cc840a5b2d7
Fisherman August <fshrmn.august@gmail.com>
2025-07-13 11:57:47 +0300

    go + 3 pages

--- server.go
+++ server.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Index Page")
+}
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "About Page")
+}
+func ContactsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "About Page")
+}
+
 func main() {
-	
+
+	fmt.Println("Sever is listening...")
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
