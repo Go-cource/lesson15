9d0f84dc253bc7ff5252813f5b81c8630d67b987
Fisherman August <fshrmn.august@gmail.com>
2025-07-13 12:55:58 +0300

    templates

--- server.go
+++ server.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/page.html")
+	MainText := fmt.Sprintf("Это мой первый сайт!!\n Всем привет \nВремя: %v", time.Now().Format("02-01-06 15:04:05"))
+	//http.ServeFile(w, r, "./static/page.html")
+	tmpl, err := template.ParseFiles("./static/page.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	tmpl.Execute(w, MainText)
 }
 
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
