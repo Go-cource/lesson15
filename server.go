095aa7e05729d268c33cefa5be7226c14bded540
Fisherman August <fshrmn.august@gmail.com>
2025-07-13 12:03:09 +0300

    3 pages

--- server.go
+++ server.go
@@ -12,10 +12,14 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "About Page")
 }
 func ContactsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "About Page")
+	fmt.Fprint(w, "Contacts Page")
 }
 
 func main() {
+
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/about", AboutHandler)
+	http.HandleFunc("/contacts", ContactsHandler)
 
 	fmt.Println("Sever is listening...")
 	http.ListenAndServe("127.0.0.1:8080", nil)
