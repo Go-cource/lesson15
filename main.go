3ca350f8e401f0985c3c6ae253cc3ffbe34f889b
Fisherman August <fshrmn.august@gmail.com>
2025-12-07 13:12:28 +0300

    task

--- main.go
+++ main.go
@@ -1,66 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"time"
 )
 
-// fmt.Errorf()
-// errors.New()
-
-// error
-type TimeError struct {
-	Time time.Time
-	Err  error //io.EOF
+// LabelError описывает ошибку с дополнительной меткой.
+type LabelError struct {
+	Label string // метка должна быть в верхнем регистре
+	Err   error
 }
 
-func (te *TimeError) Error() string {
-
-	return fmt.Sprintf("[%v]: %v", te.Time.Format("2006/01/02 15:04:05"), te.Err)
-}
-
-func (te *TimeError) Unwrap() error {
-	return te.Err
-}
-
-func NewTimeError(err error) error {
-	return &TimeError{
-		Time: time.Now(),
-		Err:  err,
-	}
-}
-
-func ReadFile(fileName string) (string, error) {
-	data, err := os.ReadFile(fileName) //err = os.FileNotExist = "Error file not Exist"
-	if err != nil {
-		return "", NewTimeError(err)
-	}
-	return string(data), nil
-}
-
-// func Is(err, target error) bool {}
-// io.EOF
-// err == io.EOF - fail
+// добавьте методы Error() и NewLabelError(label string, err error) error
+// ...
 
 func main() {
-	_, err := ReadFile("config.txt")
+	_, err := os.ReadFile("mytest.txt") //net.Dial() err-> NewLableEror("net", err)
 	if err != nil {
-		// if errors.Is(err, os.ErrNotExist) {
-
-		// 	fmt.Println("File Created.")
-		// } else {
-		// 	fmt.Println("такой ошибки не знаю и не обрабатываю: ", err)
-		// 	os.Exit(1)
-		// }
-		var myErr *TimeError
-		if errors.As(err, &myErr) {
-			fmt.Println(myErr.Error())
-			fmt.Println(myErr.Err)
-			fmt.Println(myErr.Time)
-
-		}
-
+		err = NewLabelError("file", err)
 	}
+	fmt.Println(err)
+	// должна выводить
+	// [FILE] open mytest.txt: no such file or directory
 }
