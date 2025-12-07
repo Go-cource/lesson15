eb7c298ee73456caba0125c752f85e6b3eb591f7
Fisherman August <fshrmn.august@gmail.com>
2025-12-07 11:40:47 +0300

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+// fmt.Errorf()
+// errors.New()
+
+// error
+type TimeError struct {
+	Time time.Time
+	Err  error
+}
+
+func (te *TimeError) Error() string {
+	return fmt.Sprintf("%v: %v", te.Time.Format("2006/01/02 15:04:05"), te.Err)
+}
+
+func NewTimeError(err error) error {
+	return &TimeError{
+		Time: time.Now(),
+		Err:  err,
+	}
+}
+
+func ReadFile(fileName string) (string, error) {
+	data, err := os.ReadFile(fileName) //err = os.FileNotExist = "Error file not Exist"
+	if err != nil {
+		return "", err // TimeError {Time: 11:26, err: os.FileNotExist}
+	}
+	return string(data), nil
+}
+
+func main() {
+
+}
